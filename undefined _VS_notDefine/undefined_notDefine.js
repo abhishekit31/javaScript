d6c42727bd21012c5a52024ec44a09699727dc50
abhishek <abhishekit31@gmail.com>
2024-11-04 19:38:19 +0530

    Initial commit

--- undefined _VS_notDefine/undefined_notDefine.js
+++ undefined _VS_notDefine/undefined_notDefine.js
@@ -2,7 +2,17 @@ console.log(num); // undefined
 var num = 10;
 console.log(num) // 10
 
+
+if(num === undefined){
+    console.log("num is undefined");
+}
+else{
+    console.log("num is not undefined");
+}
+
 /**
+ * UNdefind variable that has been declared but not assigned a value is undefined 
+ * 
  * undefined is a special keyword in javaScript anf it is like a placeholder.
  * 
  * in the javaScript whenever the programhas start executing the javascript engine is creeted a global execution context.
@@ -14,5 +24,7 @@ console.log(num) // 10
 console.log(x); // Uncaught ReferenceError: x is not defined
 
 /**
+ * A variable that has not been declared at all is not defined
+ * Not define is something which is not allocated in the memory.
  * he at line no. 14 it will thorw an ReferenceError that is x is not define because we trying to access the x that is no were in the moemory allocation.
  */
