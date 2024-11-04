2f8339fa29bf121d4194192e1d8c6ebb432ba701
abhishek <abhishekit31@gmail.com>
2024-11-04 10:52:56 +0530

    Initail commit

--- Hoisting/hoisting.js
+++ Hoisting/hoisting.js
@@ -28,16 +28,33 @@
  * 
  * we can put some value in it  and we can access it without any error
  * 
- * in javascript before it excute the code by javscript engine it crate a memory isnide it 
+ * In javascript before start excuting the code by javscript engine it crate a memory is allocated isnide it stored some special keywprd known as undefined
  */
 getName();     // Abhishek
 console.log(a);  // undefined
+console.log(getName) // return the whole getNmae function.
 
 var a = 7;
 
 function getName(){
     console.log("Abhishek")
 }
+
+/**
+ *  If we are declaring the function as a arrow funtion and try to access it before it initialize it it will throw the error "getName is not a function"
+ *  becouse now arrow funtion is treated as a variable not a function.
+ */
+
+// getName();     // Uncaught TypeError: getName is not a function
+// console.log(a);  // undefined
+
+// var a = 7;
+
+// var getName = () =>{
+//     console.log("Abhishek")
+// }
+// getName(); //Abhishek
+
 
 /**
  * before and after initalize the funtion if we are trying to console this getName it will log the whole function to the console
