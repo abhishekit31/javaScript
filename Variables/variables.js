c9c221c944a0a243295ced7113e96cce2612e723
abhishek <abhishekit31@gmail.com>
2024-11-07 10:53:25 +0530

    Initial commit

---
+++ Variables/variables.js
@@ -0,0 +1,14 @@
+const accountId = "121";
+let accountEmail = "abhishekit31@gmail.com";
+var accountPass = "12345";
+accountCity = "New Delhi";
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
+
+// accountId = "1212"; not allowed
+
+accountEmail = "abhishek@gmail.com";
+accountPass = "125";
+accountCity = "Delhi";
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
