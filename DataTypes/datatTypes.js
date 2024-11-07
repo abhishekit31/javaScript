2b2a9b58e9fc0f25bf48bbbeda97d05baef7c85b
abhishek <abhishekit31@gmail.com>
2024-11-07 13:15:07 +0530

    Initial commit

---
+++ DataTypes/datatTypes.js
@@ -0,0 +1,20 @@
+let isLogged = false;
+let state;
+let name = "Abhishek";
+let num = 10;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false
+// mull => standrard value
+// undefined =>
+// symbol => unique
+
+// Object => data types
+
+console.log(typeof "Abhishek"); // string
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
