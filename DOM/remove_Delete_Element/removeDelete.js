1c47b4975dc812f0aa6c6cd7a289773be4b088ca
abhishek <abhishekit31@gmail.com>
2024-11-10 19:57:36 +0530

    Initial commit

---
+++ DOM/remove_Delete_Element/removeDelete.js
@@ -0,0 +1,52 @@
+const div = document.createElement("div");
+div.id = "main";
+
+const ul = document.createElement("ul");
+ul.id = "unorder";
+
+document.body.appendChild(div);
+div.appendChild(ul);
+
+console.log(div);
+
+// =========basic way==========
+
+function addLanguage(lang) {
+  const li = document.createElement("li");
+  li.innerText = lang;
+
+  const ul = document.getElementById("unorder");
+  ul.appendChild(li);
+}
+addLanguage("javaScript");
+
+// =========optimzed way==========
+
+function addOptimizeLang(lang) {
+  const li = document.createElement("li");
+  const linode = document.createTextNode(lang);
+  li.appendChild(linode);
+
+  const ul = document.getElementById("unorder");
+  ul.appendChild(li);
+}
+addOptimizeLang("java");
+addOptimizeLang("python");
+addOptimizeLang("ruby");
+
+// =========Edit the list==========
+
+const editLi = document.querySelector("li:nth-child(2");
+editLi.innerHTML = "mojo";
+
+// =========Edit the list by optimized way==========
+
+const editLi = document.querySelector("li:nth-child(2");
+const newLi = document.createElement("li");
+newLi.textContent = "mojo";
+editLi.replaceWith(newLi);
+
+// =========remove List==========
+
+const removeList = document.querySelector("li:last-child");
+removeList.remove();
