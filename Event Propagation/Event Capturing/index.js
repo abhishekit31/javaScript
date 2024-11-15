85ff2a2b12d1a5baee3f3088e5afe9ec914bd775
abhishek <abhishekit31@gmail.com>
2024-11-15 13:08:03 +0530

    Initial commit

--- Event Propagation/Event Capturing/index.js
+++ Event Propagation/Event Capturing/index.js
@@ -1,21 +1,9 @@
-document.querySelector("#grandparent").addEventListener(
-  "click",
-  () => {
-    console.log("Grandparent Clicked !");
-  },
-  true
-);
-document.querySelector("#parent").addEventListener(
-  "click",
-  () => {
-    console.log("Parent Clicked !");
-  },
-  true
-);
-document.querySelector("#child").addEventListener(
-  "click",
-  () => {
-    console.log("Child Clicked !");
-  },
-  true
-);
+document.querySelector("#grandparent").addEventListener("click", () => {
+  console.log("Grandparent Clicked !");
+});
+document.querySelector("#parent").addEventListener("click", () => {
+  console.log("Parent Clicked !");
+});
+document.querySelector("#child").addEventListener("click", () => {
+  console.log("Child Clicked");
+});
